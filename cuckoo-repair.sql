6e742b6ceae8357d12fc98196968854650095381
rutine <rutine@163.com>
2024-05-02 09:57:25 +0800

    升级guava quartz依赖包

--- cuckoo-repair.sql
+++ cuckoo-repair.sql
@@ -2,3 +2,4 @@ update `syspl_district` set district_id = 0 where district_name = '地区树';
 update `syspl_module_menu` set module_id = 0 where mod_name = '模块树';
 update `uum_organ` set org_id = 0 where org_simple_name = '总部';
 update `uum_role` set role_id = 0 where role_name = '无角色用户';
+update `uum_user` set user_id = 0 where user_code = 'admin';
