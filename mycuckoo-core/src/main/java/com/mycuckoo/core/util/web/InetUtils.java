4dc594683395e8c3a78eaea838cf79c519a1ab4e
rutine <rutine@163.com>
2024-08-11 21:03:50 +0800

    要点 1. 调整pom依赖scope配置

--- mycuckoo-core/src/main/java/com/mycuckoo/core/util/web/InetUtils.java
+++ mycuckoo-core/src/main/java/com/mycuckoo/core/util/web/InetUtils.java
@@ -1,10 +1,13 @@
 package com.mycuckoo.core.util.web;
 
 import com.mycuckoo.core.util.StrUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Optional;
 
 /**
  * @author rutine
@@ -58,45 +61,45 @@ public abstract class InetUtils {
         return ip;
     }
 
-//    /**
-//     * 获取客户端真实ip
-//     * @param request request
-//     * @return 返回ip
-//     */
-//    public static String getIp(ServerHttpRequest request) {
-//        HttpHeaders headers = request.getHeaders();
-//        String ipAddress = headers.getFirst("x-forwarded-for");
-//        if (ipAddress == null || ipAddress.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
-//            ipAddress = headers.getFirst("Proxy-Client-IP");
-//        }
-//        if (ipAddress == null || ipAddress.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
-//            ipAddress = headers.getFirst("WL-Proxy-Client-IP");
-//        }
-//        if (ipAddress == null || ipAddress.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
-//            ipAddress = Optional.ofNullable(request.getRemoteAddress())
-//                    .map(address -> address.getAddress().getHostAddress())
-//                    .orElse("");
-//            if (IP_LOCAL.equals(ipAddress)) {
-//                // 根据网卡取本机配置的IP
-//                try {
-//                    InetAddress inet = InetAddress.getLocalHost();
-//                    ipAddress = inet.getHostAddress();
-//                } catch (UnknownHostException e) {
-//                    // ignore
-//                }
-//            }
-//        }
-//
-//        // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
-//        if (ipAddress != null && ipAddress.length() > IP_LEN) {
-//            int index = ipAddress.indexOf(",");
-//            if (index > 0) {
-//                ipAddress = ipAddress.substring(0, index);
-//            }
-//        }
-//
-//        return ipAddress;
-//    }
+    /**
+     * 获取客户端真实ip
+     * @param request request
+     * @return 返回ip
+     */
+    public static String getIp(ServerHttpRequest request) {
+        HttpHeaders headers = request.getHeaders();
+        String ipAddress = headers.getFirst("x-forwarded-for");
+        if (ipAddress == null || ipAddress.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = headers.getFirst("Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = headers.getFirst("WL-Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || IP_UNKNOWN.equalsIgnoreCase(ipAddress)) {
+            ipAddress = Optional.ofNullable(request.getRemoteAddress())
+                    .map(address -> address.getAddress().getHostAddress())
+                    .orElse("");
+            if (IP_LOCAL.equals(ipAddress)) {
+                // 根据网卡取本机配置的IP
+                try {
+                    InetAddress inet = InetAddress.getLocalHost();
+                    ipAddress = inet.getHostAddress();
+                } catch (UnknownHostException e) {
+                    // ignore
+                }
+            }
+        }
+
+        // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if (ipAddress != null && ipAddress.length() > IP_LEN) {
+            int index = ipAddress.indexOf(",");
+            if (index > 0) {
+                ipAddress = ipAddress.substring(0, index);
+            }
+        }
+
+        return ipAddress;
+    }
 
     public static long[] ipToLong(String ip) {
         if (ip.contains(":")) {
